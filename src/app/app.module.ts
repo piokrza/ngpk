6cf826e232e25882fb46863f09c7c4860796ae91
Piotr Krzaczkowski <slimhannya@gmail.com>
2023-03-19 11:29:10 +0100

    feat(app): install tailwind & NgRx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
-import { NgModule } from '@angular/core';
+import { isDevMode, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from '@app/app-routing.module';
 import { AppComponent } from '@app/app.component';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { ButtonModule } from 'primeng/button';
 
 const declarations: Array<any> = [AppComponent];
-const imports: Array<any> = [BrowserModule, AppRoutingModule, ButtonModule];
+const imports: Array<any> = [
+  BrowserModule,
+  AppRoutingModule,
+  ButtonModule,
+
+  // NgRx
+  StoreModule.forRoot({}, {}),
+  EffectsModule.forRoot(),
+  StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+];
 const providers: Array<any> = [];
 
 @NgModule({ declarations, imports, providers, bootstrap: [AppComponent] })
