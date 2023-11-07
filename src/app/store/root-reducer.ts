8b7acfdd816dd62c101a1707d30f933b2e822c25
Krzaczkowski Piotr (BL) <piotr.krzaczkowski@cyfra.gov.pl>
2023-11-07 22:55:16 +0100

    update angular

--- src/app/store/root-reducer.ts
+++ src/app/store/root-reducer.ts
@@ -2,13 +2,13 @@ import { InjectionToken } from '@angular/core';
 import { ActionReducerMap } from '@ngrx/store';
 
 import * as fromAuth from '#store/auth';
-import * as fromIncomes from '#store/cash-flow';
+import * as fromCashFlow from '#store/cash-flow';
 import * as fromCategories from '#store/categories';
 import * as fromTasker from '#store/tasker';
 
 export interface AppState {
   [fromCategories.FeatureKey]: fromCategories.State;
-  [fromIncomes.FeatureKey]: fromIncomes.State;
+  [fromCashFlow.FeatureKey]: fromCashFlow.State;
   [fromAuth.FeatureKey]: fromAuth.State;
   [fromTasker.FeatureKey]: fromTasker.State;
 }
@@ -16,7 +16,7 @@ export interface AppState {
 export const ROOT_REDUCERS = new InjectionToken<ActionReducerMap<AppState>>('ROOT_REDUCERS', {
   factory: (): ActionReducerMap<AppState> => ({
     [fromCategories.FeatureKey]: fromCategories.Reducer,
-    [fromIncomes.FeatureKey]: fromIncomes.Reducer,
+    [fromCashFlow.FeatureKey]: fromCashFlow.Reducer,
     [fromAuth.FeatureKey]: fromAuth.Reducer,
     [fromTasker.FeatureKey]: fromTasker.Reducer,
   }),
