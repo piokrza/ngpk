18c9c75eb78b9442691210b2618f83ba0758dec4
Krzaczkowski Piotr (BL) <piotr.krzaczkowski@cyfra.gov.pl>
2023-12-06 16:55:31 +0100

    fix: remove visibilityState model

--- src/app/common/models/index.ts
+++ src/app/common/models/index.ts
@@ -1,5 +1,4 @@
 export * from './language.model';
-export * from './visibility-state.model';
 export * from './labeled-data.model';
 export * from './nullable.model';
 export * from './feature.model';
--- src/app/common/models/visibility-state.model.ts
+++
@@ -1,1 +0,0 @@
-export type VisibilityState = 'visible' | 'notVisible';
