4532ae0602d421028621e7c5d26cbceb4fccdd85
Krzaczkowski Piotr (BL) <piotr.krzaczkowski@cyfra.gov.pl>
2023-12-14 13:59:44 +0100

    fix(dashboard): remove facade

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -1,7 +1,13 @@
 import { ChangeDetectionStrategy, Component, OnInit, inject } from '@angular/core';
-import { first } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { filter, tap } from 'rxjs';
 
-import { DashboardFacade } from '#dashboard/data-access';
+import { AuthApi } from '#auth/services';
+import { AuthActions } from '#store/auth';
+import { CashFlowActions } from '#store/cash-flow';
+import { CategoriesActions } from '#store/categories';
+import { DriveActions } from '#store/drive';
+import { TaskerActions } from '#store/tasker';
 
 @Component({
   selector: 'ctrl-dashboard',
@@ -13,9 +19,23 @@ import { DashboardFacade } from '#dashboard/data-access';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DashboardComponent implements OnInit {
-  private readonly dashboardFacade = inject(DashboardFacade);
+  private readonly store: Store = inject(Store);
+  private readonly authApi: AuthApi = inject(AuthApi);
 
   public ngOnInit(): void {
-    this.dashboardFacade.initializeUserData$().pipe(first()).subscribe();
+    this.authApi.authState$
+      .pipe(
+        filter(Boolean),
+        tap(({ uid }) => {
+          this.store.dispatch(AuthActions.loadUserData());
+          this.store.dispatch(CategoriesActions.getCategories());
+          this.store.dispatch(TaskerActions.getTasks({ uid }));
+          this.store.dispatch(TaskerActions.getNotes({ uid }));
+          this.store.dispatch(CashFlowActions.getExpenses({ uid }));
+          this.store.dispatch(CashFlowActions.getIncomes({ uid }));
+          this.store.dispatch(DriveActions.getFiles({ uid }));
+        })
+      )
+      .subscribe();
   }
 }
--- src/app/features/dashboard/dashboard.module.ts
+++ src/app/features/dashboard/dashboard.module.ts
@@ -1,17 +1,15 @@
 import { CommonModule } from '@angular/common';
-import { NgModule, Provider } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ChartModule } from 'primeng/chart';
 import { ProgressSpinnerModule } from 'primeng/progressspinner';
 
 import { DashboardRoutingModule, DashboardComponent } from '#dashboard/.';
-import { DashboardFacade } from '#dashboard/data-access';
 import { LayoutModule } from '#layout/.';
 import { ContainerComponent } from '#shared/components';
 
 const declarations = [DashboardComponent];
 const imports = [CommonModule, DashboardRoutingModule, ChartModule, LayoutModule, CardModule, ProgressSpinnerModule, ContainerComponent];
-const providers: Provider[] = [DashboardFacade];
 
-@NgModule({ declarations, imports, providers })
+@NgModule({ declarations, imports })
 export default class DashboardModule {}
