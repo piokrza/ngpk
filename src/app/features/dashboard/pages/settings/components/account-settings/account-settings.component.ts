1dea7c1513a5047ca5cf3c899834f189242d9a8f
Krzaczkowski Piotr (BL) <piotr.krzaczkowski@cyfra.gov.pl>
2024-01-22 09:01:10 +0100

    feat: add dashboard path fragment enum

--- src/app/features/dashboard/pages/settings/components/account-settings/account-settings.component.ts
+++ src/app/features/dashboard/pages/settings/components/account-settings/account-settings.component.ts
@@ -2,6 +2,8 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 import { PrimeIcons } from 'primeng/api';
 
+import { DashboardPathFragments } from '#dashboard/enums';
+
 @Component({
   selector: 'org-account-settings',
   templateUrl: './account-settings.component.html',
@@ -9,4 +11,5 @@ import { PrimeIcons } from 'primeng/api';
 })
 export class AccountSettingsComponent {
   readonly PrimeIcons: typeof PrimeIcons = PrimeIcons;
+  readonly DashboardPathFragments: typeof DashboardPathFragments = DashboardPathFragments;
 }
