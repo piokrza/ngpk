46244523136f1ac0b5bd0200fc511001cbbf3782
Piotr Krzaczkowski <slimhannya@gmail.com>
2024-02-24 00:07:26 +0100

    add basic drag and drop

--- src/app/features/tasker/components/board/board.component.ts
+++ src/app/features/tasker/components/board/board.component.ts
@@ -1,9 +1,28 @@
+import { CdkDragDrop, DragDropModule, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+const imports = [DragDropModule];
 
 @Component({
   selector: 'org-board',
-  template: `<h1>Board działa</h1>`,
+  templateUrl: './board.component.html',
+  styleUrl: './board.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
+  imports,
 })
-export class BoardComponent {}
+export class BoardComponent {
+  todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  doing = ['This is doing example'];
+
+  done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  onDrop(event: CdkDragDrop<string[]>): void {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data, event.container.data, event.previousIndex, event.currentIndex);
+    }
+  }
+}
