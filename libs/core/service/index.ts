a25b42e20e4af6207bbbbd4e34d21330676dcc64
Piotr Krzaczkowski <slimhannya@gmail.com>
2024-04-12 23:59:44 +0200

    fix: restore theme service

--- libs/core/service/index.ts
+++ libs/core/service/index.ts
@@ -1,5 +1,5 @@
-export * from './theme.service';
 export * from './menu.service';
 export * from './toast.service';
 export * from './db-subscription.service';
 export * from './title.service';
+export * from './theme.service';
