95830320432b33371aa9e70b2f33389d44d522b2
Piotr Krzaczkowski <slimhannya@gmail.com>
2024-04-21 18:34:38 +0200

    remove unnecessary files

--- libs/email/config/app-config.ts
+++ libs/email/config/app-config.ts
@@ -1,10 +1,9 @@
 import { InjectionToken } from '@angular/core';
 import { environment } from 'apps/email-client/src/environments/environment';
 
-import { config } from '@ngpk/email/enum';
 import { AppConfig } from '@ngpk/email/model';
 
-export const APP_SERVICE_CONFIG: InjectionToken<AppConfig> = new InjectionToken<AppConfig>(config.APP_CONFIG);
+export const APP_SERVICE_CONFIG: InjectionToken<AppConfig> = new InjectionToken<AppConfig>('app-config');
 
 export const appConfig: AppConfig = {
   BASE_URL: environment.BASE_URL,
