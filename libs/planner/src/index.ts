4a01966d42d959bbb8a9f0fd5e440fabd825f389
Piotr Krzaczkowski <slimhannya@gmail.com>
2024-04-23 20:20:09 +0200

    add planner-client & planner lib

---
+++ libs/planner/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/planner/planner.component';
---
+++ libs/planner/src/lib/planner/planner.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'ngpk-planner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './planner.component.html',
+  styleUrl: './planner.component.css',
+})
+export class PlannerComponent {}
