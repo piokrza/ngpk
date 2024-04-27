4e17b7d06a708902d57d7559a3394e40dbb38acf
Piotr Krzaczkowski <slimhannya@gmail.com>
2024-04-27 13:08:36 +0200

    feat(email-client): add auth guard

--- libs/email/feature/auth/auth-routing.module.ts
+++ libs/email/feature/auth/auth-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { authGuard } from '@ngpk/email/guard';
-
 const routes: Routes = [
   {
     path: '',
@@ -11,12 +9,10 @@ const routes: Routes = [
   },
   {
     path: 'signin',
-    canMatch: [authGuard],
     loadComponent: async () => (await import('@ngpk/email/shared/components')).SigninComponent,
   },
   {
     path: 'signup',
-    canMatch: [authGuard],
     loadComponent: async () => (await import('@ngpk/email/shared/components')).SignupComponent,
   },
   {
--- libs/email/guard/auth.guard.ts
+++ libs/email/guard/auth.guard.ts
@@ -1,6 +1,17 @@
-import { CanMatchFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanMatchFn, Router } from '@angular/router';
+import { map, switchMap, tap } from 'rxjs';
+
+import { AuthService } from '@ngpk/email/service';
+import { AuthStateService } from '@ngpk/email/state/auth';
 
 export const authGuard: CanMatchFn = () => {
-  //TODO: implement guard
-  return true;
+  const router = inject(Router);
+  const authService = inject(AuthService);
+  const authStateService = inject(AuthStateService);
+
+  return authService.checkAuth$().pipe(
+    switchMap(() => authStateService.select('isSignedIn').pipe(map((isSignedIn) => Boolean(isSignedIn)))),
+    tap((isSignedIn: boolean) => !isSignedIn && void router.navigate(['']))
+  );
 };
