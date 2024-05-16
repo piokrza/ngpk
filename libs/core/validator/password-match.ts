1aadc3cf43a43616eb079a6fcf6285f6143821c3
Piotr Krzaczkowski <97129887+piokrza@users.noreply.github.com>
2024-05-16 22:21:09 +0200

    Wrap all organizer features into one lib (#32)

--- libs/core/validator/password-match.ts
+++ libs/core/validator/password-match.ts
@@ -1,9 +1,9 @@
 import { AbstractControl } from '@angular/forms';
 
-export function passwordMatchValidator(controlNameOne: string, controlNameTwo: string) {
+export function passwordMatchValidator(passwordControlName: string, passwordConfirmationControlName: string) {
   return (form: AbstractControl): { passwordMismatch: boolean } | null => {
-    const passwordValue: string | undefined = form.get(controlNameOne)?.value;
-    const passwordConfirmationValue: string | undefined = form.get(controlNameTwo)?.value;
+    const passwordValue: string | undefined = form.get(passwordControlName)?.value;
+    const passwordConfirmationValue: string | undefined = form.get(passwordConfirmationControlName)?.value;
 
     return passwordValue === passwordConfirmationValue ? null : { passwordMismatch: true };
   };
