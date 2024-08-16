188eee0b1f5f5a3f04c770a24236073a7dc034c9
Piotr Krzaczkowski <slimhannya@gmail.com>
2024-08-16 14:07:20 +0200

    feat(zoneless): add angular material

--- apps/zoneless/src/app/app.config.ts
+++ apps/zoneless/src/app/app.config.ts
@@ -1,7 +1,8 @@
 import { routes } from './app.routes';
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes)],
+  providers: [provideExperimentalZonelessChangeDetection(), provideRouter(routes), provideAnimationsAsync()],
 };
--- libs/playground/component/crud/home/home.component.ts
+++ libs/playground/component/crud/home/home.component.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
+const imports = [MatButtonModule];
 
 @Component({
   selector: 'ngpk-home',
-  template: `<h1>value: {{ value() }}</h1>`,
+  template: ` <button mat-button>Basic</button>
+
+    <h1>value: {{ value() }}</h1>`,
   standalone: true,
+  imports,
 })
 export class HomeComponent {
   constructor() {
