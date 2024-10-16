bf94d9e74728445da12bd28a641264f6be041ac4
Krzaczkowski Piotr <piotr.krzaczkowski@cyfra.gov.pl>
2024-10-16 02:33:37 +0200

    remove apps jira&slack

--- apps/jira/src/main.ts
+++
@@ -1,5 +0,0 @@
-import { AppComponent } from './app/app.component';
-import { appConfig } from './app/app.config';
-import { bootstrapApplication } from '@angular/platform-browser';
-
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
--- apps/slack/src/app/app.config.ts
+++
@@ -1,7 +0,0 @@
-import { appRoutes } from './app.routes';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(appRoutes)],
-};
